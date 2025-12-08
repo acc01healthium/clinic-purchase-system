ca18e9bde225fdaeafe8a101a8f51dce85a9fa13
acc01healthium <acc.01.healthium@gmail.com>
2025-12-08 17:28:49 +0800

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,5 @@
+// ./js/supabase.js
+const SUPABASE_URL = "'https://utwhtjtgwryeljgwlwzm.supabase.co'o";
+const SUPABASE_ANON_KEY = "@supabase/supabase-js";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
