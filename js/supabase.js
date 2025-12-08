c558b30c45d8034775c4233bfaf851cb96a1d505
acc01healthium <acc.01.healthium@gmail.com>
2025-12-08 21:17:11 +0800

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
 // ./js/supabase.js
 const SUPABASE_URL = "'https://utwhtjtgwryeljgwlwzm.supabase.co'o";
-const SUPABASE_ANON_KEY = "@supabase/supabase-js";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0d2h0anRnd3J5ZWxqZ3dsd3ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxNTkxNDQsImV4cCI6MjA4MDczNTE0NH0.SexZh_JV9IUT5cL7o6KO-bh6D50aFkZUrhZVf4_fNbs";
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
