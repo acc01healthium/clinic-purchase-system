90fa8588e57347d4ae2c2ed6e57079c3d6a00627
acc01healthium <acc.01.healthium@gmail.com>
2025-12-10 13:00:53 +0800

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -5,6 +5,9 @@
 const SUPABASE_URL = "https://utwhtjtgwryeljgwlwzm.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0d2h0anRnd3J5ZWxqZ3dsd3ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxNTkxNDQsImV4cCI6MjA4MDczNTE0NH0.SexZh_JV9IUT5cL7o6KO-bh6D50aFkZUrhZVf4_fNbs";
 
-window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+// 讓其他 JS 可使用
+window.supabaseClient = supabase;
 
 console.log("前台 Supabase 初始化成功");
