ba0349f34bc4cb312b9da1ed59b067673198645b
acc01healthium <acc.01.healthium@gmail.com>
2025-12-11 09:43:44 +0800

    Update supabase.js

--- admin/js/supabase.js
+++ admin/js/supabase.js
@@ -2,7 +2,7 @@
 const SUPABASE_URL = "https://utwhtjtgwryeljgwlwzm.supabase.co";
 
 // ⚠️ service_role 只有後台使用，請勿公開
-const SUPABASE_SERVICE_ROLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0d2h0anRnd3J5ZWxqZ3dsd3ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxNTkxNDQsImV4cCI6MjA4MDczNTE0NH0.SexZh_JV9IUT5cL7o6KO-bh6D50aFkZUrhZVf4_fNbs";
+const SUPABASE_SERVICE_ROLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0d2h0anRnd3J5ZWxqZ3dsd3ptIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2NTE1OTE0NCwiZXhwIjoyMDgwNzM1MTQ0fQ.mV9Zv0rSJT8c9xr7o0ywoKZLCohNAuBvUJzz8mGrrFE";
 
 // 建立後台 Supabase client（擁有完整權限）
 window.supabaseAdmin = supabase.createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
