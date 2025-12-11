64ee6b7bc508ecff1fb7cc17f65b6a85eb920865
acc01healthium <acc.01.healthium@gmail.com>
2025-12-11 09:48:41 +0800

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,7 @@
+// 前台：使用 anon key（安全，可公開）
 const SUPABASE_URL = "https://utwhtjtgwryeljgwlwzm.supabase.co";
+
+// ⚠️ 前台一定要用 anon key（不能用 service_role）
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0d2h0anRnd3J5ZWxqZ3dsd3ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxNTkxNDQsImV4cCI6MjA4MDczNTE0NH0.SexZh_JV9IUT5cL7o6KO-bh6D50aFkZUrhZVf4_fNbs";
 
 window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
