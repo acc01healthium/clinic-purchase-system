e687901479e3eee69e951efcf97f12c0e75ac3b4
acc01healthium <acc.01.healthium@gmail.com>
2025-12-14 00:32:43 +0800

    Update supabase.js

--- admin/js/supabase.js
+++ admin/js/supabase.js
@@ -5,14 +5,13 @@ const SUPABASE_URL = "https://utwhtjtgwryeljgwlwzm.supabase.co";
 const SUPABASE_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0d2h0anRnd3J5ZWxqZ3dsd3ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxNTkxNDQsImV4cCI6MjA4MDczNTE0NH0.SexZh_JV9IUT5cL7o6KO-bh6D50aFkZUrhZVf4_fNbs";
 
-window.supabaseClient = supabase.createClient(
+window.supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY,
   {
     auth: {
-      persistSession: true,
+      persistSession: true,   // ✅ 一定要 true
       autoRefreshToken: true,
-      detectSessionInUrl: false,
     },
   }
 );
